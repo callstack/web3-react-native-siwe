4b8d90c923c63a6705cd15d17132275b73d0d612
Thiago Brezinski <thiagobrez@gmail.com>
2024-06-27 14:11:20 +0100

    chore: restructure repo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,31 @@
-import cors from 'cors';
-import express from 'express';
-import { generateNonce, SiweMessage } from 'siwe';
+import cors from "cors";
+import express from "express";
+import { generateNonce, SiweMessage } from "siwe";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/nonce', function (_, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send(generateNonce());
+app.get("/nonce", function (_, res) {
+  res.setHeader("Content-Type", "text/plain");
+
+  const nonce = generateNonce();
+  console.log("[BACKEND] Generated nonce:", nonce);
+
+  res.send(nonce);
 });
 
-app.post('/verify', async function (req, res) {
-    const { message, signature } = req.body;
-    const siweMessage = new SiweMessage(message);
-    try {
-        await siweMessage.verify({ signature });
-        res.send(true);
-    } catch {
-        res.send(false);
-    }
+app.post("/verify", async function (req, res) {
+  const { message, signature } = req.body;
+  const siweMessage = new SiweMessage(message);
+  try {
+    await siweMessage.verify({ signature });
+    console.log("[BACKEND] SIWE Message valid: true");
+    res.send(true);
+  } catch {
+    console.log("[BACKEND] SIWE Message valid: false");
+    res.send(false);
+  }
 });
 
 app.listen(3000);
