a36fb2339e1aa86e393646f6eb26e737fae3a14c
Thiago Brezinski <thiagobrez@gmail.com>
2024-06-27 15:24:47 +0100

    feat: use viem in the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
-import { generateNonce, SiweMessage } from "siwe";
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
+import { generateSiweNonce } from "viem/siwe";
 
 const app = express();
 app.use(express.json());
@@ -9,23 +11,34 @@ app.use(cors());
 app.get("/nonce", function (_, res) {
   res.setHeader("Content-Type", "text/plain");
 
-  const nonce = generateNonce();
+  // Generate nonce
+  const nonce = generateSiweNonce();
+
   console.log("[BACKEND] Generated nonce:", nonce);
 
+  // Send back response
   res.send(nonce);
 });
 
 app.post("/verify", async function (req, res) {
   const { message, signature } = req.body;
-  const siweMessage = new SiweMessage(message);
-  try {
-    await siweMessage.verify({ signature });
-    console.log("[BACKEND] SIWE Message valid: true");
-    res.send(true);
-  } catch {
-    console.log("[BACKEND] SIWE Message valid: false");
-    res.send(false);
-  }
+
+  // Create publicClient
+  const publicClient = createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  });
+
+  // Verify whether the signed message coming from the frontend is valid for the given address
+  const valid = await publicClient.verifySiweMessage({
+    message,
+    signature,
+  });
+
+  console.log(`[BACKEND] SIWE Message valid: ${valid}`);
+
+  // Send back response
+  res.send(valid);
 });
 
 app.listen(3000);
